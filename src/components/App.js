a0f726997b99d53e8084f627e3fa3e02bc367380
jayakanthb <jayakanth.b1995@gmail.com>
2020-07-30 16:39:22 +0530

    changes in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube, { baseParams } from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube, { baseParams } from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+//
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   componentDidMount() {
-    this.onTermSubmit('buildings');
+    this.onTermSubmit("buildings");
   }
 
-  onTermSubmit = async term => {
-    const response = await youtube.get('/search', {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
-          ...baseParams ,
-        q: term
-      }
+        ...baseParams,
+        q: term,
+      },
     });
 
     this.setState({
       videos: response.data.items,
-      selectedVideo: response.data.items[0]
+      selectedVideo: response.data.items[0],
     });
   };
 
-  onVideoSelect = video => {
+  onVideoSelect = (video) => {
     this.setState({ selectedVideo: video });
   };
 
